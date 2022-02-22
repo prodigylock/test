dee62837ed0533c5f070fe10b3b851149ba95544
prodigylock <prodigylock@gmail.com>
2022-02-22 13:37:12 +0200

    removed for

--- src/App.java
+++ src/App.java
@@ -3,8 +3,5 @@ public class App {
         System.out.println("Hello, Git!");
         System.out.println("Goodbye, Git!");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
     }
 }
